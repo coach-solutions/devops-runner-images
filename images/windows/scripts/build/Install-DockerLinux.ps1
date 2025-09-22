2c86baa225c272db64ded178291d31a026d9d0b7
Mads Lundemann <mlu@coachsolutions.com>
2025-09-22 10:45:55 +0200

    DEV-3821: Try to make sure WSL doesn't shutdown.

--- images/windows/scripts/build/Install-DockerLinux.ps1
+++ images/windows/scripts/build/Install-DockerLinux.ps1
@@ -47,6 +47,7 @@ $startupScript | Out-File -FilePath C:\DockerLinux\Startup.ps1
 
 $runScript = @'
 & "C:\Program Files\WSL\wsl.exe" '-u' 'root' '-d' 'Docker' 'mount' '--make-shared' '/mnt/c'
+& "C:\Program Files\WSL\wsl.exe" '-u' 'root' '-d' 'Docker' '-e' 'dbus-launch' 'true'
 & "C:\Program Files\WSL\wsl.exe" '-u' 'root' '-d' 'Docker' '-e' '/bin/bash'
 '@
 $runScript | Out-File -FilePath C:\DockerLinux\Run.ps1
